ed44ab3ef194657f3031482e0440776ecd3af835
Anna Kravets <annakravets1906@gmail.com>
2023-03-04 20:54:09 +0200

    1. Створено галерею

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const galerryImages = document.querySelector(".gallery");
+const galleryMarkUp = createGallery(galleryItems);
+
+galerryImages.innerHTML = galleryMarkUp;
+
+function createGallery(galleryItems) {
+  const imagesMarkUp = galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+    </a>
+    </div>`;
+    })
+    .join("");
+  return imagesMarkUp;
+}
 
 console.log(galleryItems);
